d566fed0ed0a88f576e37c8ae54dc82097f2e9c5
yuchanghong <yuchanghong@grgbanking.com>
2021-12-09 16:32:53 +0800

    fix: helloworld控制器

--- src/main/java/com/fish1208/controller/HelloWorldController.java
+++ src/main/java/com/fish1208/controller/HelloWorldController.java
@@ -3,35 +3,12 @@ package com.fish1208.controller;
 import com.fish1208.bean.HelloWorld;
 import com.fish1208.common.response.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
-import org.bouncycastle.util.encoders.Hex;
-import org.chainmaker.pb.common.ContractOuterClass;
-import org.chainmaker.pb.common.Request;
 import org.chainmaker.pb.common.ResultOuterClass;
 import org.chainmaker.sdk.ChainClient;
 import org.chainmaker.sdk.SdkException;
-import org.chainmaker.sdk.User;
-import org.chainmaker.sdk.utils.CryptoUtils;
-import org.chainmaker.sdk.utils.SdkUtils;
-import org.chainmaker.sdk.utils.Utils;
-import org.chainmaker.sdk.utils.UtilsException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.Utf8String;
-import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.utils.Numeric;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,35 +24,12 @@ public class HelloWorldController {
     private static long syncResultTimeout = 10000;
 
     private static final String CONTRACT_NAME = "helloworld";
-    private static String ADDRESS = "";
 
     @Autowired
     private ChainClient chainClient;
 
-    @Autowired
-    @Qualifier("adminUser1")
-    private User adminUser1;
-
-    @Autowired
-    @Qualifier("adminUser2")
-    private User adminUser2;
-
-    @Autowired
-    @Qualifier("adminUser3")
-    private User adminUser3;
-
-    private void makeAddrFromCert() {
-        try {
-            ADDRESS = CryptoUtils.makeAddrFromCert(chainClient.getClientUser().getTlsCertificate());
-            log.info("ADDRESS={}", ADDRESS);
-        } catch (UtilsException e) {
-            e.printStackTrace();
-        }
-    }
-
     @PostMapping(value = "/set")
-    public Result<?> set(@RequestBody HelloWorld hello) throws IOException, SdkException{
-//        makeAddrFromCert();
+    public Result<?> set(@RequestBody HelloWorld hello) {
 
         Map<String, byte[]> params = new HashMap<>();
         params.put("n",  hello.getN().getBytes());
@@ -83,8 +37,7 @@ public class HelloWorldController {
 
         ResultOuterClass.TxResponse responseInfo = null;
         try {
-            responseInfo = chainClient.invokeContract(CONTRACT_NAME,
-                    method, null, params, rpcCallTimeout, syncResultTimeout);
+            responseInfo = chainClient.invokeContract(CONTRACT_NAME, method, null, params, rpcCallTimeout, syncResultTimeout);
         } catch (SdkException e) {
             e.printStackTrace();
         }
@@ -93,8 +46,8 @@ public class HelloWorldController {
     }
 
     @GetMapping(value = "/get")
-    public Result<?> get() throws IOException, SdkException{
-//        makeAddrFromCert();
+    public Result<?> get() {
+
         String method = "get";
         ResultOuterClass.TxResponse responseInfo = null;
         try {
